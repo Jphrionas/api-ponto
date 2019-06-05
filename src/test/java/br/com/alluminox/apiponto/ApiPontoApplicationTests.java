c51ab854c8569aaf6c9d5a2b0351384c8e0ac48c
Jphrionas <jhm.homme@gmail.com>
2019-06-05 13:29:17 -0300

    Starting the project

---
+++ src/test/java/br/com/alluminox/apiponto/ApiPontoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.alluminox.apiponto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiPontoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
